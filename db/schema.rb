ac90ef526226d38243ee73f89cc0a054a8971d9d
muslih <muslihaqq@gmail.com>
2023-04-01 03:32:44 +0800

    Add migration: add sleep time in minutes
    Signed-off-by: muslih <muslihaqq@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_31_143626) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_185558) do
   create_table "sleep_records", force: :cascade do |t|
     t.datetime "clock_in"
     t.datetime "clock_out"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "sleep_time"
     t.index ["user_id"], name: "index_sleep_records_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+5.times do
+  u = User.create(
+    name: Faker::Name.name,
+    handle: Faker::Internet.username(specifier: 5..10)
+  )
+  puts "User successfully created #{u.name}:#{u.handle}"
+end
