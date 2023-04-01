670f678d97f1e2ad4b54c03026fbc34647b7e5bb
muslih <muslihaqq@gmail.com>
2023-04-02 06:46:09 +0800

    Adjust exception handler

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,13 +2,20 @@ module ExceptionHandler
   # provides the more graceful `included` method
   extend ActiveSupport::Concern
 
+  Invalid = Class.new(StandardError)
+  NotAuthorized = Class.new(StandardError)
+
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
+      render json: { error: e.message }, status: :not_found
     end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
+    rescue_from ApplicationController::Invalid do |e|
+      render json: { error: e.message }, status: :unprocessable_entity
+    end
+
+    rescue_from ApplicationController::NotAuthorized do |e|
+      render json: { error: e.message }, status: :unauthorized
     end
   end
 end
